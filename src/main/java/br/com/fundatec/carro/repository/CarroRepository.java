e822a4bc0c59a8df0a1b35dd1f2c5bc4c3a31d9c
Guilherme Karam <guilherme-klm@hotmail.com>
2019-11-22 22:26:49 -0300

    Adicionado CarroRepository

---
+++ src/main/java/br/com/fundatec/carro/repository/CarroRepository.java
@@ -0,0 +1,16 @@
+package br.com.fundatec.carro.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class CarroRepository {
+
+    public List<String> listarCarros () {
+        List<String> carros = Arrays.asList("Celta", "Fusca", "Ferrari", "Uno");
+        return carros;
+    }
+
+}
--- src/main/java/br/com/fundatec/carro/service/CarroService.java
+++ src/main/java/br/com/fundatec/carro/service/CarroService.java
@@ -1,15 +1,20 @@
 package br.com.fundatec.carro.service;
 
+import br.com.fundatec.carro.repository.CarroRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CarroService {
 
-    public List<String> listarCarros () {
-        List<String> carros = Arrays.asList("Celta", "Fusca", "Ferrari", "Uno");
-        return carros;
+    private final CarroRepository carroRepository;
+
+    public CarroService(CarroRepository carroRepository) {
+        this.carroRepository = carroRepository;
+    }
+
+    public List<String> listarCarros() {
+        return carroRepository.listarCarros();
     }
 }
